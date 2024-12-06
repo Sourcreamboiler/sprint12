b1e2eb63265147f1b07059f9c53d3e87dad63f6e
Nikolay Grishanin <grishaninnikm@gmail.com>
2024-12-06 17:39:56 +0000

    done

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	ParcelStatusRegistered = "registered" //sosjoiss
+	ParcelStatusRegistered = "registered" //sosjoissebgrbgfb
 	ParcelStatusSent       = "sent"
 	ParcelStatusDelivered  = "delivered"
 )
